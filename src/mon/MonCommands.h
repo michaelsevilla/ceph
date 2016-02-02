03ce260ecc22e9181117a0257c5b026b31a92d0f
Michael Sevilla <mikesevilla3@gmail.com>
2016-02-02 11:25:42 -0800

    mantle: store balancer in RADOS, balancer version in MDSMap - add docs and sample balancer (greedy-spill)
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- src/mon/MonCommands.h
+++ src/mon/MonCommands.h
@@ -330,7 +330,7 @@ COMMAND("mds set_max_mds " \
 	"set max MDS index", "mds", "rw", "cli,rest")
 COMMAND("mds set " \
 	"name=var,type=CephChoices,strings=max_mds|max_file_size"
-	"|allow_new_snaps|inline_data|allow_multimds|allow_dirfrags " \
+	"|allow_new_snaps|inline_data|allow_multimds|allow_dirfrags|balancer " \
 	"name=val,type=CephString "					\
 	"name=confirm,type=CephString,req=false",			\
 	"set mds parameter <var> to <val>", "mds", "rw", "cli,rest")
