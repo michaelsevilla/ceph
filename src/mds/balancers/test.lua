26386d587e57195e09a607f250687d88693bcc93
Michael Sevilla <mikesevilla3@gmail.com>
2016-07-25 13:15:13 -0700

    mantle: write balancer policies in Lua
    Introduces Mantle, a programmable metadata load balancer. Policies for making
    migration decisions are written in Lua but the Migrator and Balancer modules
    still do fragmentation and migration. If the Lua balancer fails, control falls
    back to the original balancer implementation.
    
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

---
+++ src/mds/balancers/test.lua
@@ -0,0 +1,11 @@
+metrics = {"auth.meta_load", "all.meta_load", 
+           "req_rate", "queue_len", "cpu_load_avg"}
+for i=0, #mds do
+  s = "MDS"..i..": < "
+  for j=1, #metrics do
+    s = s..metrics[j].."="..mds[i][metrics[j]].." "
+  end
+  BAL_LOG(0, s..">")
+end
+
+return {51, 52, 54}
